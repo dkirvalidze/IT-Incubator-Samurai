d41af124723e51fdad7b40976860cbb009081a69
dkirvalidze <dkirva94@gmail.com>
2022-10-21 16:38:38 +0400

    11

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,32 @@
 import React from 'react';
 import './App.css';
-import Header from "./Header";
-import Technologies from "./Technologies";
 
-const App = function() {
-  return (
-    <div className="App">
-     Hello, samurai! Let's go!
-        <Header />
-        <Technologies />
-    </div>
-  );
+
+const App = function () {
+    return (
+        <div className="App">
+            <header className={"header"}>
+                <img
+                    src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/2300px-React-icon.svg.png"
+                    alt=""/>
+            </header>
+            <nav className={"nav"}>
+                <div>
+                    <a>Profile</a>
+                </div>
+                <div>
+                    <a>Messages</a>
+                </div>
+                <div>
+                    <a>News</a>
+                </div>
+            </nav>
+            <div className={"content"}>
+                <img src={"https://cdn.techjuice.pk/wp-content/uploads/2019/01/react.jpeg"}/>
+                Main content
+            </div>
+        </div>
+    );
 }
 
 export default App;
--- src/Header.tsx
+++
@@ -1,8 +0,0 @@
-const Header = () => {
-    return (
-        <div>
-            <h1>I need to learn these technologies:</h1>
-        </div>
-    );
-};
-export default Header;
