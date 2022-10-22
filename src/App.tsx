575be3fd3a9fe955a88e9e7dbabbdd013e04e049
dkirvalidze <dkirva94@gmail.com>
2022-10-22 12:22:01 +0400

    12 - Components

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,16 @@
 import React from 'react';
 import './App.css';
+import { Header } from './components/Header';
+import { Navbar } from './components/Navbar';
+import { Profile } from './components/Profile';
 
 
 const App = function () {
     return (
         <div className="App">
-            <header className={"header"}>
-                <img
-                    src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/2300px-React-icon.svg.png"
-                    alt=""/>
-            </header>
-            <nav className={"nav"}>
-                <div>
-                    <a>Profile</a>
-                </div>
-                <div>
-                    <a>Messages</a>
-                </div>
-                <div>
-                    <a>News</a>
-                </div>
-            </nav>
-            <div className={"content"}>
-                <img src={"https://cdn.techjuice.pk/wp-content/uploads/2019/01/react.jpeg"}/>
-                Main content
-            </div>
+            <Header />
+            <Navbar />
+            <Profile />
         </div>
     );
 }
---
+++ src/components/Profile.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+export const Profile = () => {
+    return (
+        <div className={"content"}>
+            <img src={"https://cdn.techjuice.pk/wp-content/uploads/2019/01/react.jpeg"} alt={"head"} />
+            <div>
+                Avatar + Description
+            </div>
+            <div>
+                My posts
+            </div>
+            <div>
+                New posts
+            </div>
+            <div>
+                post 1
+            </div>
+            <div>
+                post 2
+            </div>
+        </div>
+    );
+}
