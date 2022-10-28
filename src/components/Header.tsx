487d712af34f5d8ccf3c93a520b5cf970d705c77
dkirvalidze <dkirva94@gmail.com>
2022-10-28 13:54:19 +0400

    14 - CSS Modules

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,8 @@
 import React from "react";
-
+import s from'./Header.module.css';
 export const Header = () => {
     return (
-        <header className={"header"}>
+        <header className={s.header}>
             <img
                 src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/2300px-React-icon.svg.png"
                 alt="" />
--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,8 +1,8 @@
 import React from "react";
-
+import s from'./Profile.module.css'
 export const Profile = () => {
     return (
-        <div className={"content"}>
+        <div className={s.content}>
             <img src={"https://cdn.techjuice.pk/wp-content/uploads/2019/01/react.jpeg"} alt={"head"} />
             <div>
                 Avatar + Description
@@ -10,7 +10,7 @@ export const Profile = () => {
             <div>
                 My posts
             </div>
-            <div>
+            <div className={s.item}>
                 New posts
             </div>
             <div>
