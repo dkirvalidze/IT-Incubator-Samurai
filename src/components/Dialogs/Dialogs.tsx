db0030e10b571b6161d96c2188d0918860d41885
dkirvalidze <dkirva94@gmail.com>
2022-10-29 13:48:17 +0400

    21 - Dialogs page markup

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -3,8 +3,29 @@ import s from './Dialogs.module.css';
 
 export const Dialogs = () => {
     return (
-        <div>
-            Dialogs
+        <div className={s.dialogs}>
+            <div className={s.dialogsItems}>
+                <div className={s.dialog + ' ' + s.active}>
+                    Dimych
+                </div>
+                <div className={s.dialog}>
+                    Andrey
+                </div>
+                <div className={s.dialog}>
+                    Sveta
+                </div>
+                <div className={s.dialog}>
+                    Sasha
+                </div>
+                <div className={s.dialog}>
+                    Viktor
+                </div>
+            </div>
+            <div className={s.messages}>
+                <div className={s.message}>Hi</div>
+                <div className={s.message}>How is your IT-Kamasutra?</div>
+                <div className={s.message}>Yo</div>
+            </div>
         </div>
     )
 }
