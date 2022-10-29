1582f9dc74f8e730351490c8a53197a9fc21dcd4
dkirvalidze <dkirva94@gmail.com>
2022-10-29 11:16:42 +0400

    18 - Dialogs page

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import { Dialogs } from './components/Dialogs/Dialogs';
 import { Header } from './components/Header/Header';
 import { Navbar } from './components/Navbar/Navbar';
 import { Profile } from './components/Profile/Profile';
@@ -10,7 +11,10 @@ const App = function () {
         <div className="App">
             <Header />
             <Navbar />
-            <Profile />
+            <div className='App-content'>
+                <Dialogs />
+                {/* <Profile /> */}
+            </div>
         </div>
     );
 }
